27bd2db85d4d898e6be208fd201e8a708282e4a9
Jussi Nieminen <jussi@jivinn.com>
2021-02-12 19:36:39 +0100

    serializing actual canonical s-expressions

--- src/c3res/shared/csexp.cljs
+++ src/c3res/shared/csexp.cljs
@@ -12,22 +12,41 @@
             :string [#(.set (js/Uint8Array. (.-buffer %1)) (.encode (TextEncoder.) %3) %2) #(.-length (.encode (TextEncoder.) %))]
             :buffer [#(.set (js/Uint8Array. (.-buffer %1)) %3 %2) #(.-length %)]})  
 
+(defn- get-length [element]
+  (let [lenfn (last (TYPES (:type element)))]
+    (lenfn (:value element))))
+
 (defn- insert-element [buf element offset]
-  (let [typeinfo (TYPES (:type element))
-        writefun (first typeinfo)
-        val (:value element)
-        size ((last typeinfo) val) ]
+  (let [writefun (first (TYPES (:type element)))
+        size (get-length element)
+        val (:value element)]
     (writefun buf offset val)
     (+ offset size)))
 
 (defn- buf-size [data]
-  (let [lenfn #(last (TYPES (:type %)))]
-    (reduce #(+ %1 ((lenfn %2) (:value %2))) 0 data)))
+  (reduce #(+ %1 (get-length %2)) 0 data))
 
-(defn encode [data]
+(defn serialize [data]
   (let [buf (js/ArrayBuffer. (buf-size data))
         view (js/DataView. buf)]
     (loop [element (first data) remaining (rest data) offset 0]
       (if element
         (recur (first remaining) (rest remaining) (insert-element view element offset))
         buf))))
+
+(defn- add-length [len elements]
+  (conj elements {:type :string :value (str len)} {:type :string :value ":"}))
+
+(defn- encode-internal [sexp current-elements]
+  (loop [current (first sexp) remaining (rest sexp) elements current-elements]
+    (if (some? current)
+      (recur (first remaining) (rest remaining)
+             (cond
+              (string? current) (conj (add-length (count current) elements) {:type :string :value current})
+              (instance? js/Uint8Array current) (conj (add-length (.-length current) elements) {:type :buffer :value current})
+              (seq? current) (encode-internal current (conj elements {:type :string :value "("}))))
+      (conj elements {:type :string :value ")"})))) 
+
+(defn encode [sexp]
+  (serialize (encode-internal sexp [{:type :string :value "("}])))
+
--- test/c3res/shared/csexp_test.cljs
+++ test/c3res/shared/csexp_test.cljs
@@ -4,21 +4,31 @@
             [clojure.string :as s]
             [c3res.shared.csexp :as csexp]))
 
-(deftest test-csexp-simple
-  (let [array (csexp/encode [{:type :int32 :value 3} {:type :int32 :value -333}])]
+(deftest test-serialize-ints
+  (let [array (csexp/serialize [{:type :uint32 :value 3} {:type :int32 :value -333}])]
     (is (= (str (js/Uint8Array. array)) "0,0,0,3,255,255,254,179"))))
 
 (deftest test-strings
-  (let [array (csexp/encode [{:type :string :value "foo€bar"}])]
+  (let [array (csexp/serialize [{:type :string :value "foo€bar"}])]
     (is (= (str (js/Uint8Array. array)) "102,111,111,226,130,172,98,97,114"))))
 
 (deftest test-buffer
-  (let [array (csexp/encode [{:type :buffer :value (.from js/Uint8Array (clj->js [1 2 3 9 8 7]))}])]
+  (let [array (csexp/serialize [{:type :buffer :value (.from js/Uint8Array (clj->js [1 2 3 9 8 7]))}])]
     (is (= (str (js/Uint8Array. array)) "1,2,3,9,8,7"))))
 
 (deftest test-combined
-  (let [array (csexp/encode [{:type :buffer :value (.from js/Uint8Array (clj->js [1 2 3]))}
+  (let [array (csexp/serialize [{:type :buffer :value (.from js/Uint8Array (clj->js [1 2 3]))}
                              {:type :uint32 :value 987654}
                              {:type :string :value "€a"}
                              {:type :int32 :value -1}])]
     (is (= (str (js/Uint8Array. array)) "1,2,3,0,15,18,6,226,130,172,97,255,255,255,255"))))
+
+(defn- buf-to-str [arraybuf]
+  (.apply (.-fromCharCode js/String) nil (js/Uint8Array. arraybuf)))
+
+(deftest test-encode-simple
+  (is (= (buf-to-str (csexp/encode '("foo" "bar" "z"))) "(3:foo3:bar1:z)")))
+
+(deftest test-nested-exprs
+  (is (= (buf-to-str (csexp/encode '("this" ("is" ("nested!!!!!"))))) "(4:this(2:is(11:nested!!!!!)))"))
+  )
