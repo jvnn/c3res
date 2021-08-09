b6a9c06c52396fa6e4ec077eba6d968361eb1dd2
Jussi Nieminen <jussi@jivinn.com>
2021-08-09 09:03:02 +0200

    improved arguments parsing

--- src/c3res/client/core.cljs
+++ src/c3res/client/core.cljs
@@ -5,8 +5,8 @@
             [c3res.client.storage :as storage]
             [clojure.string :as s]
             [cljs.nodejs :as node]
-            [cljs.core.async :as async :refer [<! >! chan close!]])
-  (:require-macros [cljs.core.async.macros :refer [go]]))
+            [cljs.core.async :as async :refer [<! >! put! chan close!]])
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
 
 (node/enable-util-print!)
 (def prompt (node/require "prompt"))
@@ -59,39 +59,54 @@
           (print "No master key detected, creating a new one.")
           (<! (keystore/create-master-key {} create-password-interactive)))))))
 
+(defn- access [path]
+  (let [c (chan)]
+    (.access fs path #(if %1 (put! c false) (put! c true)))
+    c))
+
+(defn- validate-arg [key value args]
+  (go
+    (case key
+      :password (if (s/blank? value) (print "Invalid or missing password") true)
+      :store (if (not (<! (access value))) (print (str "Cannot find or access file '" value "' to store")) true)
+      :fetch (do (print "Fetching not yet implemented...") false)
+      :server (if (some #(contains? args %) [:store :fetch]) (print "Cannot use --server together with --store / --fetch") true)
+      :daemon (if (or (not (:server args)) (some #(contains? args %) [:store :fetch]))
+                (print "Cannot use --daemon without --server or with --store / --fetch") (do (print "Daemon mode not yet implemented...") false)))))
+
 (defn- validate-args [args]
-  (doseq [key (keys args)]
-    (let [value (args key)]
-      (case key
-        :password (when (s/blank? value) (print "Invalid or missing password") (.exit process 1))
-        :store (when (not (.accessSync fs value)) (print "Cannot find or access file to store") (.exit process 1))
-        :fetch (do (print "Fetching not yet implemented...") (.exit process 0))
-        :server (when (some #(contains? args %) [:store :fetch]) (print "Cannot use --server together with --store / --fetch") (.exit process 1))
-        :daemon (when (or (not (:server args)) (some #(contains? args %) [:store :fetch])) (print "Cannot use --daemon without --server or with --store / --fetch") (.exit process 1)))))
-  args)
+  (go-loop [option-keys (keys args)]
+    (if-let [key (first option-keys)]
+      (when (<! (validate-arg key (args key) args))
+        (recur (rest option-keys)))
+      args)))
 
 (defn- parse-args [argv]
-  (loop [args {} current argv]
+  (go-loop [args {} current argv]
     (if (not current)
-      (validate-args args)
+      (<! (validate-args args))
       (case (first current)
         "--password" (recur (assoc args :password (second current)) (nnext current))
         "--store" (recur (assoc args :store (second current)) (nnext current))
         "--fetch" (recur (assoc args :fetch (second current)) (nnext current))
         "--server" (recur (assoc args :server true) (next current))
         "--daemon" (recur (assoc args :daemon true) (next current))
-        nil))))
+        (do (print "Invalid argument" (first current)) (.exit process 1))))))
 
 (defn main [& argv]
   (go
     (<! (sod/init))
     (print (str "running c3res client with arguments " (s/join " " argv)))
     (go
-      (let [args (parse-args argv)
-            master-key (<! (get-or-create-master-key (:password args)))]
-        (print (str "got master key " master-key))))
-    (.listen (.createServer http) 3000) ; For now just to have an event loop running...
-    ))
+      (when-let [args (<! (parse-args argv))]
+        (let [master-key (<! (get-or-create-master-key (:password args)))]
+          (cond
+            ; CONTINUE HERE:
+            ;    - implement fetching from cache to check that we store stuff successfully
+            ;    - implement adding labels
+            ;    - implement (poor man's...?) mime type detection (or check for libraries)
+            (:store args) (.readFile fs (:store args) #(when-not %1 (go (<! (cache/new-shard %2 {"test" "label"} "text/plain" (chan 1) {} master-key)))))
+            (:server args) (.listen (.createServer http) 3000)))))))
 
 (set! *main-cli-fn* main)
 
