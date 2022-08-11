8a0877a23e52a606880daf004bd6a0b2860fe4b2
Jussi Nieminen <jussi@jivinn.com>
2022-08-11 22:52:31 +0200

    example of handling database migration; not pushed to master as I don't think this change is really needed

--- src/c3res/server/db.cljs
+++ src/c3res/server/db.cljs
@@ -5,17 +5,45 @@
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (def sqlite (node/require "sqlite3"))
+(def CURRENT_DB_VERSION 1)
+
 (def create-table-cmds
   ["CREATE TABLE IF NOT EXISTS shards (shard_id VARCHAR(64) PRIMARY KEY, timestamp INT, author VARCHAR(64));"
    "CREATE TABLE IF NOT EXISTS caps (shard_id VARCHAR(64) REFERENCES shards(shard_id), cap_shard_id VARCHAR(64) REFERENCES shards(shard_id)); "
-   "CREATE TABLE IF NOT EXISTS labels (label_key TEXT, label_value TEXT, shard_id VARCHAR(64) REFERENCES shards(shard_id));"
-   "CREATE TABLE IF NOT EXISTS relations (shard_id VARCHAR(64) REFERENCES shards(shard_id), other_id VARCHAR(64) REFERENCES shards(shard_id), relation_name TEXT;"])
+   "CREATE TABLE IF NOT EXISTS labels (label_key TEXT, label_value TEXT, shard_id VARCHAR(64) REFERENCES shards(shard_id), timestamp INT);"
+   "CREATE TABLE IF NOT EXISTS relations (shard_id VARCHAR(64) REFERENCES shards(shard_id), other_id VARCHAR(64) REFERENCES shards(shard_id), relation_name TEXT;"
+   (str "PRAGMA user_version = " CURRENT_DB_VERSION ";")])
+
+
+(defn- get-version [db]
+  (let [c (chan)]
+    (.get db "PRAGMA user_version" #(if %1 (put! c false) (put! c ((js->clj %2) "user_version"))))
+    c))
 
 (defn- create-tables [db]
   (let [c (chan)]
     (doseq [cmd create-table-cmds]
       (.exec db cmd #(put! c (nil? %))))
     c))
+
+(defn- migrate-0 [db]
+  (let [c (chan)]
+    (.exec db (str
+               "ALTER TABLE labels ADD COLUMN timestamp INT;"
+               "UPDATE labels SET timestamp = shard.timestamp FROM (SELECT timestamp, shard_id FROM shards) AS shard WHERE labels.shard_id = shard.shard_id;"
+               "PRAGMA user_version = 1;")
+          #(do (when % (print "Failed to migrate database from version 0:" %)) (close! c)))
+    c))
+
+; TODO: a unit test with different DB versions (one per version with some dummy data)
+; checking that in the end all of those have migrated to the latest schema with meaningful data
+(defn- migrate-db [version db]
+  (go
+    (cond
+      (= version 0) (<! (migrate-0 db))
+      ; add further migratio code here, make sure all migration steps are executed when difference is multiple versions
+      )
+    db))
 
 (defn open-db [path]
   (go
@@ -24,16 +52,16 @@
           db (Database. path (bit-or (.-OPEN_READWRITE sqlite) (.-OPEN_CREATE sqlite)) #(put! c (nil? %)))]
       (if (<! c)
         (if (<! (create-tables db))
-          db
+          (migrate-db (<! (get-version db)) db)
           false)
         false))))
 
 (defn store-shard-metadata [db shard-id timestamp author labels]
   (let [shards-stmt (.prepare db "INSERT INTO shards VALUES (?, ?, ?)")
-        labels-stmt (.prepare db "INSERT INTO labels VALUES (?, ?, ?)")]
+        labels-stmt (.prepare db "INSERT INTO labels VALUES (?, ?, ?, ?)")]
     (.run shards-stmt shard-id timestamp author (fn [error] (when error (print "Error when storing metadata to shards table:" error))))
     (doseq [label-key (keys labels)]
-      (.run labels-stmt label-key (labels label-key) shard-id (fn [error] (when error (print "Error when storing labels:" error)))))))
+      (.run labels-stmt label-key (labels label-key) shard-id timestamp (fn [error] (when error (print "Error when storing labels:" error)))))))
 
 (defn query-labels [db label value]
   (let [c (chan)
